49bb3d8d0476a899c9258e711c9d04dd05e7a1f8
Sanjeev Kumar <Sanjeev.kumar@goavege.com>
2019-12-12 22:56:45 +0530

    VID 001 complete

--- main.go
+++ main.go
@@ -1,17 +1,51 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+// Article single
+type Article struct {
+	Title   string `json:"title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+// Articles list of Article
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+
+	articles := Articles{
+		Article{
+			Title:   "Test Title",
+			Desc:    "Test Description",
+			Content: "Hello World",
+		},
+
+		Article{
+			Title:   "Test Title2",
+			Desc:    "Test Description2",
+			Content: "Hello Sanjeev",
+		},
+	}
+
+	fmt.Println("Endpoint Hit:: All Articles")
+	json.NewEncoder(w).Encode(articles)
+}
+
 func handleHomePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit::Landing Page")
+
 	fmt.Fprintf(w, "HomePage Endpoint hi Sanjeev!!")
 }
 
 func handleRequest() {
 	http.HandleFunc("/", handleHomePage)
+	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
